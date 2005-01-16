c9f3644b6f93fd22baa6bd1a0783829276eb0b73
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-01-16 18:43:00 +0000

    Added a file remotely
    git-svn-id: https://plugins.svn.wordpress.org/moderate-trackbacks/branches/WP-1.3-alpha-3+@370 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ moderate-trackbacks.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Moderate Trackbacks
+Plugin URI: http://mtdewvirus.com/archives/2005/01/06/wordpress-plugins-trackback-and-pingback-moderation/
+Description: Automatically puts all trackbacks into the comment moderation queue.
+Author: Nick Momrik
+Version: 1.1
+Author URI: http://mtdewvirus.com/
+*/ 
+
+function moderate_trackback($comment_ID) {
+global $wpdb;
+
+$wpdb->query("UPDATE $wpdb->comments SET comment_approved='0' WHERE comment_ID=$comment_ID");
+
+return $comment_ID;
+}
+
+add_action('trackback_post', 'moderate_trackback');
+?>
