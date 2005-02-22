14c04153d0e3bac1925b9034f436384c038c9fce
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-02-22 00:37:52 +0000

    Moderate the trackback before it is committed to the DB
    git-svn-id: https://plugins.svn.wordpress.org/moderate-trackbacks/branches/wp-1.5@829 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- moderate-trackbacks.php
+++ moderate-trackbacks.php
@@ -2,19 +2,20 @@
 /*
 Plugin Name: Moderate Trackbacks
 Plugin URI: http://mtdewvirus.com/code/
-Description: Automatically puts all trackbacks into the comment moderation queue.
+Description: Puts trackbacks not marked as 'spam' into moderation.
 Author: Nick Momrik
-Version: 1.12
+Version: 1.2
 Author URI: http://mtdewvirus.com/
 */ 
 
-function mdv_moderate_trackback($comment_ID) {
-	global $wpdb;
-	
-	$wpdb->query("UPDATE $wpdb->comments SET comment_approved='0' WHERE comment_ID=$comment_ID AND comment_approved='1'");
-	
-	return $comment_ID;
+function mdv_moderate_trackback($approved) {
+	global $wp_query;
+
+	if (1 == $approved && $wp_query->is_trackback)
+		$approved = 0;
+
+	return $approved;
 }
 
-add_action('trackback_post', 'mdv_moderate_trackback');
+add_filter('pre_comment_approved', 'mdv_moderate_trackback');
 ?>
